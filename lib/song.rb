46da9d3718dacad57af3de1257aec3de308d7c7f
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-02-08 07:32:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,17 @@ class Song
   def save
     self.class.all << self
   end
+  
+  Song.create_from_filename
+  song = self.new
+  song.artist_name = (name.split(" - ")[0])
+  song.name = (name.split(" - ")[1].chomp(".mp3"))
+  @@all << song 
+  song
+  end
+
+  
+  Song.destroy_all
+  end
 
 end
